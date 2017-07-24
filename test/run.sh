ee49bf8dc80d13e9300ab89fa379d0a1a7b86b5a
ykhadilkar-rei <ykhadilkar@reisys.com>
2017-07-24 12:39:41 -0400

    updating tests

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
+cd ../
 
 mvn clean package
 ret=$?
@@ -10,27 +10,11 @@ exit $ret
 fi
 rm -rf target
 
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
-cd ../initial
-
 mvn clean compile
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
 
 exit
