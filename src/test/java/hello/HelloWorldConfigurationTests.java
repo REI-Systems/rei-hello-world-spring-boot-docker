533a0fd2375103ba0d017e8e3ef42ff8cac27ac9
ykhadilkar-rei <ykhadilkar@reisys.com>
2017-08-23 11:55:01 -0400

    Addng cobertura plugin

--- src/test/java/hello/HelloWorldConfigurationTests.java
+++ src/test/java/hello/HelloWorldConfigurationTests.java
@@ -15,8 +15,6 @@
  */
 package hello;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +26,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -45,6 +45,13 @@ public class HelloWorldConfigurationTests {
         ResponseEntity<String> entity = restTemplate
                 .getForEntity("http://localhost:" + this.port + "/", String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+    @Test
+    public void testGreetingText() throws Exception {
+        ResponseEntity<String> entity = restTemplate
+                .getForEntity("http://localhost:" + this.port + "/", String.class);
+        assertEquals("Hello World!", entity.getBody());
     }
 
 }
